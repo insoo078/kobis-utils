0f14255b0a755e6f12920ef97006697896fa5890
insoo078 <insoo078@192.168.2.10>
2015-04-03 23:25:33 +0900

    update

--- KobisUtils/src/org/kobic/kobis/utils/ReadExcel.java
+++ KobisUtils/src/org/kobic/kobis/utils/ReadExcel.java
@@ -14,7 +14,9 @@ import org.kobic.kobis.main.dao.KobisDAOService;
 import org.kobic.kobis.main.services.AbstractKobisServices;
 import org.kobic.kobis.main.services.CellStrainServices;
 import org.kobic.kobis.main.services.CommonServices;
+import org.kobic.kobis.main.services.DnaRnaProteinDerivativesServices;
 import org.kobic.kobis.main.services.ExtractServices;
+import org.kobic.kobis.main.services.StrainServices;
 import org.kobic.kobis.mybatis.factory.MyBatisConnectionFactory;
 
 public class ReadExcel{
@@ -111,9 +113,11 @@ public class ReadExcel{
 				
 				AbstractKobisServices ks = (AbstractKobisServices) obj;
 				
-//				if( ks instanceof CommonServices);
-				if( ks instanceof CellStrainServices);
-				else								continue;
+				if( ks instanceof CommonServices)	continue;
+//				if( ks instanceof CellStrainServices);
+//				if( ks instanceof StrainServices);
+//				if( ks instanceof DnaRnaProteinDerivativesServices);
+//				else								continue;
 
 				ks.readRecordsInSheet();
 			}
@@ -135,8 +139,8 @@ public class ReadExcel{
 //		-o /Users/lion/Desktop -i /Users/lion/git/kobis-utils/KobisUtils/sample/20150227_KOBIS_정보연계표준안_수정_ver9_한국식물추출물은행.xlsx -header INS00001
 
 		String[][] params = new String[][]{
-//				{"-o", "/Users/lion/Desktop", "-i", "/Users/lion/git/kobis-utils/KobisUtils/sample/20150227_KOBIS_정보연계표준안_수정_ver9_한국식물추출물은행.xlsx", "-header", "INS00001"},
-				{"-o", "/Users/lion/Desktop", "-i", "/Users/lion/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_수정_ver9_KCTC_Sample.xlsx", "-header", "INS00002"},
+				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/20150227_KOBIS_정보연계표준안_수정_ver9_한국식물추출물은행.xlsx", "-header", "INS00001"},
+				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_수정_ver9_KCTC_Sample.xlsx", "-header", "INS00002"},
 //				{"-o", "/Users/lion/Desktop", "-i", "/Users/lion/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_수정_국가영장류센터_KOBIS수정본.xlsx", "-header", "INS00003"},
 //				{"-o", "/Users/lion/Desktop", "-i", "/Users/lion/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_유전자은행_KOBIS수정본.xlsx", "-header", "INS00004"}
 //				{"-o", "/Users/lion/Desktop", "-i", "/Users/lion/git/kobis-utils/KobisUtils/sample/KOBIS_해외소재센터_201503 06 1차 데이터_IRMRC.xlsx", "-header", "INS00005"}
