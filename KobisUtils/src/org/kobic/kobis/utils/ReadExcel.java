5e816d683e24c38606407efb854dfa2ae3985e3e
insoo078 <insoo078@192.168.2.10>
2015-04-05 13:08:37 +0900

    update

--- KobisUtils/src/org/kobic/kobis/utils/ReadExcel.java
+++ KobisUtils/src/org/kobic/kobis/utils/ReadExcel.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.lang.reflect.Constructor;
 
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
@@ -25,6 +26,8 @@ import org.kobic.kobis.main.services.StrainServices;
 import org.kobic.kobis.mybatis.factory.MyBatisConnectionFactory;
 
 public class ReadExcel{
+	private static Logger logger = Logger.getLogger(ReadExcel.class); 
+
 	private SqlSessionFactory sessionFactory;
 	
 	private String inFile;
@@ -119,11 +122,11 @@ public class ReadExcel{
 				AbstractKobisServices ks = (AbstractKobisServices) obj;
 				
 				if( ks instanceof CommonServices)	continue;
-				if( ks instanceof IndividualServices )	continue;
-				if( ks instanceof CellStrainServices)	continue;
-				if( ks instanceof StrainServices)	continue;
-				if( ks instanceof DnaRnaProteinDerivativesServices) continue;
-				if( ks instanceof SourceServices ) continue;
+//				if( ks instanceof IndividualServices )	continue;
+//				if( ks instanceof CellStrainServices)	continue;
+//				if( ks instanceof StrainServices)	continue;
+//				if( ks instanceof DnaRnaProteinDerivativesServices) continue;
+//				if( ks instanceof SourceServices ) continue;
 //				else								continue;
 
 				ks.readRecordsInSheet();
@@ -146,22 +149,22 @@ public class ReadExcel{
 //		-o /Users/lion/Desktop -i /Users/lion/git/kobis-utils/KobisUtils/sample/20150227_KOBIS_정보연계표준안_수정_ver9_한국식물추출물은행.xlsx -header INS00001
 
 		String[][] params = new String[][]{
-//				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/20150227_KOBIS_정보연계표준안_수정_ver9_한국식물추출물은행.xlsx", "-header", "INS00001"},
-//				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_수정_ver9_KCTC_Sample.xlsx", "-header", "INS00002"},
-//				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_수정_국가영장류센터_KOBIS수정본.xlsx", "-header", "INS00003"},
+				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/20150227_KOBIS_정보연계표준안_수정_ver9_한국식물추출물은행.xlsx", "-header", "INS00001"},
+				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_수정_ver9_KCTC_Sample.xlsx", "-header", "INS00002"},
+				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_수정_국가영장류센터_KOBIS수정본.xlsx", "-header", "INS00003"},
 				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_정보연계표준안_유전자은행_KOBIS수정본.xlsx", "-header", "INS00004"},
-//				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_해외소재센터_201503 06 1차 데이터_IRMRC.xlsx", "-header", "INS00005"},
-//				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_해외소재센터_정보연계표준_20150403 2차 데이터_IRMRC.xlsx", "-header", "INS00005"}
+				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_해외소재센터_201503 06 1차 데이터_IRMRC.xlsx", "-header", "INS00005"},
+				{"-o", "/Users/insoo078/Desktop", "-i", "/Users/insoo078/git/kobis-utils/KobisUtils/sample/KOBIS_해외소재센터_정보연계표준_20150403 2차 데이터_IRMRC.xlsx", "-header", "INS00005"}
 		};
 
-		for(int i=0; i<params.length; i++) {
-			ReadExcel read = new ReadExcel();
-			try {
-				System.out.println( params[i][5] + " processing");
-				read.run( params[i] );
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
+//		for(int i=0; i<params.length; i++) {
+//			ReadExcel read = new ReadExcel();
+//			try {
+//				System.out.println( params[i][5] + " processing");
+//				read.run( params[i] );
+//			}catch(Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
 }
